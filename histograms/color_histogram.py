124ed8885cbf98e668d2fae6fe5aee108ef2adb4
Olavo Mendes <olavo.77@hotmail.com>
2021-05-07 22:01:37 -0300

    Realizados ajustes no projeto HISTOGRAMS

--- histograms/color_histogram.py
+++ histograms/color_histogram.py
@@ -5,20 +5,28 @@ import numpy as np
 image = cv2.imread(r'images\beach.png')
 
 ### COLOR ####
-image = cv2.imread(r'images\beach.png')
 cv2.imshow('Original', image)
 
 channels = cv2.split(image)
-colors = ('b', 'g', 'r')
-plt.figure(figsize=(6, 4))
-plt.title('Flattened color histogram')
-plt.xlabel('Bins')
-plt.ylabel('# of pixels')
+# colors = ('b', 'g', 'r')
+# plt.figure(figsize=(6, 4))
+# plt.title('Flattened color histogram')
+# plt.xlabel('Bins')
+# plt.ylabel('# of pixels')
 
-for (channel, color) in zip(channels, colors):
-    hist = cv2.calcHist([channel], [0], None, [256], [0, 256])
-    plt.plot(hist, color=color)
-    plt.xlim([0, 256])
+# for (channel, color) in zip(channels, colors):
+#     hist = cv2.calcHist([channel], [0], None, [256], [0, 256])
+#     plt.plot(hist, color=color)
+#     plt.xlim([0, 256])
+
+fig = plt.figure()
+
+ax = fig.add_subplot(131)
+hist = cv2.calcHist(channels[1], channels[0], [0, 1],
+                    None, [32, 32], [0, 256, 0, 256])
+p = ax.imshow(hist, interpolation='nearest')
+ax.set_title('2D color histogram for G and B')
+plt.colorbar(p)
 
 plt.tight_layout()
 plt.show()
