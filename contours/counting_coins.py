e52220899833a814a4285aa771b1e74f0d9e33e5
Olavo Mendes <olavo.77@hotmail.com>
2021-05-10 21:47:54 -0300

    Adicionado projeto COUNTING COINS

--- contours/counting_coins.py
+++ contours/counting_coins.py
@@ -12,7 +12,22 @@ edged = cv2.Canny(blurred, 30, 150)
 print('Quantidade de moedas na imagem: ', len(cnts))
 
 coins = image.copy()
+# -1 DESENHA TODOS OS CONTORNOS
 cv2.drawContours(coins, cnts, -1, (0, 255, 0), 2)
+
+for i, c in enumerate(cnts):
+    (x, y, w, h) = cv2.boundingRect(c)
+
+    print('Moeda #{}'.format(i + 1))
+    coin = image[y:y + h, x:x+w]
+    cv2.imshow('Coin', coin)
+
+    mask = np.zeros(image.shape[:2], dtype='uint8')
+    ((center_x, center_y), radius) = cv2.minEnclosingCircle(c)
+    cv2.circle(mask, (int(center_x), int(center_y)), int(radius), 255, -1)
+    mask = mask[y:y+h, x:x+w]
+
+    cv2.imshow('Masked Coin', cv2.bitwise_and(coin, coin, mask=mask))
 
 
 cv2.imshow('Image', blurred)
